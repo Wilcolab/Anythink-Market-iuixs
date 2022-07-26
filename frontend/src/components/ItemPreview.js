4c4f9c6e905d85aceb62dd128ffed3cb6763ca6f
robcrockbb <robert@vizsimply.com>
2022-07-26 08:38:07 -0400

    Ran the formatter and successfully linted the code base.

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -29,7 +29,7 @@ const ItemPreview = (props) => {
     }
   };
 
-  console.log('item', item);
+  console.log("item", item);
 
   return (
     <div
@@ -50,7 +50,7 @@ const ItemPreview = (props) => {
         <div className="d-flex flex-row align-items-center pt-2">
           <Link to={`/@${item.seller.username}`} className="flex-grow-1">
             <img
-              src={item.seller.image  || "/placeholder.png"}
+              src={item.seller.image || "/placeholder.png"}
               alt={item.seller.username}
               className="user-pic rounded-circle pr-1"
             />
